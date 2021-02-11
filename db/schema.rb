639a1ffb2cd91c201209124abac59babb81c57b3
icebox827 <icebox827@gmail.com>
2021-02-11 10:45:41 -0500

    Create models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_153315) do
+ActiveRecord::Schema.define(version: 2021_02_11_153643) do
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
