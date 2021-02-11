51f2879c00efa860d7ffd72d1f2c6c9c31ebaf42
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-02-11 10:32:17 -0600

    Create index and new view in posts, and create methods index, new, create in posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,30 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        #format.html { redirect_to root_path, notice: 'Tweeet was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        #format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @tweeet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:post)
   end
 end
