fdba13d26e9203717e9e24bb2fa0300e32dba2da
icebox827 <icebox827@gmail.com>
2021-02-11 11:37:44 -0500

    Format document

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,16 +14,17 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        #format.html { redirect_to root_path, notice: 'Tweeet was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
-        #format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @tweeet.errors, status: :unprocessable_entity }
       end
     end
   end
 
   private
+
   def post_params
     params.require(:post).permit(:post)
   end
