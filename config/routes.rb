94ec2e7bbcf5d4b6a1835bee90c78aaf79df2b70
icebox827 <icebox827@gmail.com>
2021-02-11 12:54:38 -0500

    Create Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root "posts#index"
-  devise_for :users
+  root 'posts#index'
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: %i[new create index]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_153643) do
+ActiveRecord::Schema.define(version: 2021_02_11_172925) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_153643) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
